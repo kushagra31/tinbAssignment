1adff100b9aaf2ebe364e548107ed2e40ed3df24
kushagra31 <kushagra31@users.noreply.github.com>
2025-07-27 19:24:05 +0000

    deploy: 4c8459f6d2fa8233a18908984ead71e2fc664e5a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5",
 "manifest.json": "56128fc6ec7568b99d6fc24cfa34a218",
-"flutter_bootstrap.js": "559d18312739a1655416c533f35169ff",
+"flutter_bootstrap.js": "4fa4b4eec5232ca6890d152a1453e5e0",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
